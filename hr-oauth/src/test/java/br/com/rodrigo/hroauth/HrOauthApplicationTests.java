d56adac3a8899899a36ba0fe5768dbc36d7f296e
palmeirasrodrigo <palmeirasrodrigo@hotmail.com>
2021-03-28 17:32:18 -0300

    Project hr-Oauth created

---
+++ hr-oauth/src/test/java/br/com/rodrigo/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
